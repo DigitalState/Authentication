8ee8795c1372d204699b666cba402d2096894c9a
mario-digitalstate <mario@digitalstate.ca>
2017-04-14 13:47:20 -0400

    Add registration endpoint for individual

--- src/Ds/Bundle/UserBundle/Action/Registration.php
+++ src/Ds/Bundle/UserBundle/Action/Registration.php
@@ -2,6 +2,11 @@
 
 namespace Ds\Bundle\UserBundle\Action;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
@@ -11,6 +16,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class Registration
 {
     /**
+     * @var \Symfony\Component\HttpFoundation\RequestStack
+     */
+    protected $requestStack;
+
+    /**
+     * @var \FOS\UserBundle\Model\UserManagerInterface
+     */
+    protected $userManager;
+
+    /**
+     * Constructor
+     *
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+     * @param \FOS\UserBundle\Model\UserManagerInterface $userManager
+     */
+    public function __construct(RequestStack $requestStack, UserManagerInterface $userManager)
+    {
+        $this->requestStack = $requestStack;
+        $this->userManager = $userManager;
+    }
+
+    /**
      * Registration
      *
      * @Route(path="/registration")
@@ -18,6 +45,18 @@ class Registration
      */
     public function __invoke()
     {
+        // @todo Look into using fos_user registration form
+        $request = $this->requestStack->getCurrentRequest();
+        $user = $this->userManager->createUser();
+        $user
+            ->setUsername($request->get('username'))
+            ->setEmail($request->get('username'))
+            ->setPlainPassword($request->get('password'))
+            ->setRoles([ 'ROLE_INDIVIDUAL'])
+            ->setEnabled(true);
 
+        $this->userManager->updateUser($user);
+
+        return new JsonResponse([ 'uuid' => $user->getUuid() ], Response::HTTP_CREATED);
     }
 }
