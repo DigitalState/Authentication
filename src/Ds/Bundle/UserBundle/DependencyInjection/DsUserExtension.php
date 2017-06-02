5431f49a1fafd900500d4fa69fd45d977b52bef7
mario <mario@digitalstate.ca>
2017-06-02 07:32:08 -0400

    Restructure based on new core

--- src/Ds/Bundle/UserBundle/DependencyInjection/DsUserExtension.php
+++ src/Ds/Bundle/UserBundle/DependencyInjection/DsUserExtension.php
@@ -33,6 +33,7 @@ class DsUserExtension extends Extension implements PrependExtensionInterface
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
-        $container->setParameter('ds_user.services.identities.url', $config['services']['identities']['url']);
+        // @todo Move this config -> parameters logic to a common trait in the config component bridge
+        $container->setParameter('ds_config.configs.ds_user.services.identities.url', $config['services']['identities']['url']);
     }
 }
--- src/Ds/Bundle/UserBundle/DsUserBundle.php
+++ src/Ds/Bundle/UserBundle/DsUserBundle.php
@@ -3,21 +3,10 @@
 namespace Ds\Bundle\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Ds\Bundle\UserBundle\DependencyInjection\Compiler;
 
 /**
  * Class DsUserBundle
  */
 class DsUserBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new Compiler\ConfigPass);
-    }
 }
