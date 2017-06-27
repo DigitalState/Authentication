666c4944aabe70136c033e22df320eb5b9c578f8
mario <mario@digitalstate.ca>
2017-06-26 21:45:16 -0400

    Restructure based on new core

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,6 +32,7 @@ class AppKernel extends Kernel
             new Ds\Component\Model\Bridge\Symfony\Bundle\DsModelBundle(),
             new Ds\Component\Security\Bridge\Symfony\Bundle\DsSecurityBundle(),
             new Ds\Component\Session\Bridge\Symfony\Bundle\DsSessionBundle(),
+            new Ds\Component\Translation\Bridge\Symfony\Bundle\DsTranslationBundle(),
             new Ds\Bundle\AuthenticationBundle\DsAuthenticationBundle(),
             new Ds\Bundle\RegistrationBundle\DsRegistrationBundle(),
             new Ds\Bundle\UserBundle\DsUserBundle(),
