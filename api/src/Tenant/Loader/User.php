44eeae77826384adeccda2b0baac62399c364e9e
mario-digitalstate <mario@digitalstate.ca>
2019-01-09 14:28:39 -0500

    Restructure tenant loaders

---
+++ api/src/Tenant/Loader/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Tenant\Loader;
+
+use App\EventListener\Entity\User\IdentityListener;
+use Ds\Component\Database\Util\Objects;
+use Ds\Component\Tenant\Entity\Tenant;
+
+/**
+ * Trait User
+ */
+trait User
+{
+    /**
+     * @var \App\Service\UserService
+     */
+    private $userService;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(Tenant $tenant)
+    {
+        $events = $this->userService->getManager()->getEventManager()->getListeners();
+
+        foreach ($events as $event => $listeners) {
+            foreach ($listeners as $listener) {
+                if (!is_object($listener)) {
+                    continue;
+                }
+
+                if ($listener instanceof IdentityListener) {
+                    $listener->setEnabled(false);
+                }
+            }
+        }
+
+        $data = (array) json_decode(json_encode($tenant->getData()));
+        $objects = Objects::parseFile($this->path, $data);
+        $manager = $this->userService->getCustomManager();
+
+        foreach ($objects as $object) {
+            $user = $manager->createUser();
+            $user
+                ->setUsername($object->username)
+                ->setEmail($object->email)
+                ->setPlainPassword($object->password)
+                ->setRoles($object->roles)
+                ->setOwner($object->owner)
+                ->setOwnerUuid($object->owner_uuid)
+                ->setIdentity($object->identity)
+                ->setIdentityUuid($object->identity_uuid)
+                ->setEnabled($object->enabled)
+                ->setTenant($object->tenant);
+            $manager->updateUser($user);
+        }
+    }
+}
--- api/src/Tenant/Loader/UserLoader.php
+++ api/src/Tenant/Loader/UserLoader.php
@@ -2,22 +2,15 @@
 
 namespace App\Tenant\Loader;
 
-use App\Entity\User;
-use App\EventListener\User\IdentityListener;
 use App\Service\UserService;
-use Ds\Component\Tenant\Entity\Tenant;
 use Ds\Component\Tenant\Loader\Loader;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class UserLoader
  */
 final class UserLoader implements Loader
 {
-    /**
-     * @var \App\Service\UserService
-     */
-    private $userService;
+    use User;
 
     /**
      * Constructor
@@ -27,55 +20,6 @@ final class UserLoader implements Loader
     public function __construct(UserService $userService)
     {
         $this->userService = $userService;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function load(Tenant $tenant)
-    {
-        $metadata = $this->userService->getManager()->getClassMetadata(User::class);
-
-        foreach ($metadata->entityListeners as $event => $listeners) {
-            foreach ($listeners as $key => $listener) {
-                if (IdentityListener::class === $listener['class']) {
-                    unset($metadata->entityListeners[$event][$key]);
-                }
-            }
-        }
-
-        $yml = file_get_contents('/srv/api-platform/src/App/Resources/tenant/users.yml');
-
-        // @todo Figure out how symfony does parameter binding and use the same technique
-        $yml = strtr($yml, [
-            '%identity.system.uuid%' => $tenant->getData()['identity']['system']['uuid'],
-            '%identity.admin.uuid%' => $tenant->getData()['identity']['admin']['uuid'],
-            '%user.system.uuid%' => $tenant->getData()['user']['system']['uuid'],
-            '%user.system.password%' => $tenant->getData()['user']['system']['password'],
-            '%user.admin.uuid%' => $tenant->getData()['user']['admin']['uuid'],
-            '%user.admin.password%' => $tenant->getData()['user']['admin']['password'],
-            '%business_unit.administration.uuid%' => $tenant->getData()['business_unit']['administration']['uuid'],
-            '%tenant.uuid%' => $tenant->getUuid()
-        ]);
-
-        $users = Yaml::parse($yml, YAML::PARSE_OBJECT_FOR_MAP);
-        $manager = $this->userService->getCustomManager();
-
-        foreach ($users->objects as $object) {
-            $object = (object) array_merge((array) $users->prototype, (array) $object);
-            $user = $manager->createUser();
-            $user
-                ->setUsername($object->username)
-                ->setEmail($object->email)
-                ->setPlainPassword($object->password)
-                ->setRoles($object->roles)
-                ->setOwner($object->owner)
-                ->setOwnerUuid($object->owner_uuid)
-                ->setIdentity($object->identity)
-                ->setIdentityUuid($object->identity_uuid)
-                ->setEnabled($object->enabled)
-                ->setTenant($object->tenant);
-            $manager->updateUser($user);
-        }
+        $this->path = '/srv/api/config/tenant/loader/user.yaml';
     }
 }
